d406a63c633b61667c45d4ed6ac38b3be095265b
jnhujnhu <jnhujnhu@gmail.com>
2015-12-08 20:49:44 +0800

    Foolish AI.

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -6,19 +6,49 @@ using System.Linq;
 public class AIController : Controller {
 
     IEnumerable<BallManager> balls, otherballs;
-
+	private float SmallestBallSize;
+	private BallManager SmallestBall;
+	private bool IsTargetBallsinVision;
+	public float AIVisionField = 30f;
+	private float MovementTimer;
 	void Start () {
-
+		IsTargetBallsinVision = false;
+		MovementTimer = 0;
 	}
 	
 	void Update () {
         updateBalls();
-        foreach (var ball in balls)
-        {
-            Debug.Log(ball.position); //球的位置
-            Debug.Log(ball.size); //球的大小
+		SmallestBallSize = 100000;
+        foreach (var ball in balls) {
+			if(ball.size < SmallestBallSize) {
+				SmallestBallSize = ball.size;
+				SmallestBall = ball;
+			}
         }
-        Move(new Vector3(0, 0, 0), 100f);
+        //Move(new Vector3(0, 0, 0), 100f);
+		Split (new Vector3 (0.2f, 0, 0));
+		IsTargetBallsinVision = false;
+		foreach (var otherball in otherballs) {
+			if(Vector3.Distance(otherball.position, SmallestBall.position) < AIVisionField) {
+				if(otherball.size > SmallestBall.size) {
+					Move ((SmallestBall.position - otherball.position) * 5, 100f);
+					IsTargetBallsinVision = true;
+				}
+				if(otherball.size < SmallestBall.size) {
+					Move ((otherball.position - SmallestBall.position) * 5, 100f);
+					if(otherball.size < SmallestBall.size/2) {
+						Split (otherball.position - SmallestBall.position);
+					}
+					IsTargetBallsinVision = true;
+				}
+
+			}
+		}
+		MovementTimer += Time.deltaTime;
+		if (!IsTargetBallsinVision && MovementTimer > 3) {
+			Move (new Vector3(Random.Range(1, 30), Random.Range(1, 30), Random.Range(1, 30)), 100f);
+			MovementTimer = 0;
+		}
 	}
 
     void updateBalls()
